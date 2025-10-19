a3f9183d73aa6f31c1d788c715008e21082a0e23
Olena <lisova.olenka@gmail.com>
2025-10-19 13:56:18 +0300

    Variables

--- tests/playwrite-site/playwrite-tests.spec.ts
+++ tests/playwrite-site/playwrite-tests.spec.ts
@@ -1,53 +1,23 @@
 import { test, expect } from "@playwright/test";
 
-test("OL-007 Should navigate to Docs and verify key sections are visible", async ({
-  page,
-}) => {
-  await page.goto("");
-  await expect(page.getByRole("link", { name: "Docs" })).toBeVisible();
-
-  await page.getByRole("link", { name: "Docs" }).click();
-  await expect(page.locator("h1")).toContainText("Installation");
-  await expect(
-    page.getByRole("heading", { name: "Installation" })
-  ).toBeVisible();
-  await expect(page.locator("#introduction")).toContainText("Introduction");
-  await expect(
-    page.getByRole("heading", { name: "IntroductionDirect link to" })
-  ).toBeVisible();
-  await expect(page.locator("#whats-next")).toContainText("What's next");
-  await expect(
-    page.getByRole("heading", { name: "What's nextDirect link to" })
-  ).toBeVisible();
-  await expect(page.locator("#system-requirements")).toContainText(
-    "System requirements"
-  );
-  await expect(
-    page.getByRole("heading", { name: "System requirementsDirect" })
-  ).toBeVisible();
-});
 test("OL-008 Switch between dark and light", async ({ page }) => {
+  const switchButton = page.getByRole("button", {
+    name: "Switch between dark and light",
+  });
+  const htmlLocator = page.locator("html");
+
   await page.goto("");
 
-  await expect(
-    page.getByRole("button", { name: "Switch between dark and light" })
-  ).toBeVisible();
+  await expect(switchButton).toBeVisible();
 
-  let locator = page.locator("html");
-  await expect(locator).toHaveAttribute("data-theme-choice", "system");
+  await expect(htmlLocator).toHaveAttribute("data-theme-choice", "system");
 
-  await page
-    .getByRole("button", { name: "Switch between dark and light" })
-    .click();
-  await expect(locator).toHaveAttribute("data-theme-choice", "light");
+  await switchButton.click();
+  await expect(htmlLocator).toHaveAttribute("data-theme-choice", "light");
 
-  await page
-    .getByRole("button", { name: "Switch between dark and light" })
-    .click();
-  await expect(locator).toHaveAttribute("data-theme-choice", "dark");
+  await switchButton.click();
+  await expect(htmlLocator).toHaveAttribute("data-theme-choice", "dark");
 
-  await page
-    .getByRole("button", { name: "Switch between dark and light" })
-    .click();
-  await expect(locator).toHaveAttribute("data-theme-choice", "system");
+  await switchButton.click();
+  await expect(htmlLocator).toHaveAttribute("data-theme-choice", "system");
 });
