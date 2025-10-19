faad99f102dc7ecbc06d04d5b100a213dbd985e8
Olena <lisova.olenka@gmail.com>
2025-10-19 14:47:39 +0300

    variables 5

--- tests/demo-qa-site/checkboxes.spec.ts
+++ tests/demo-qa-site/checkboxes.spec.ts
@@ -6,40 +6,42 @@ test.describe(
   { tag: ["@smoke", "@regression", "@ui", "@functional", "@positive"] },
   async () => {
     test("OL-130 Check Checkboxes with Xpath", async ({ page }) => {
-      await page.goto("/checkbox");
-      await expect(
-        page.locator("//h1[contains(text(),'Check Box')]")
-      ).toBeVisible();
+      const checkboxTitle = page.locator("//h1[contains(text(),'Check Box')]");
+      const expandAllButton = page.locator(
+        "//button[@aria-label='Expand all']"
+      );
+      const homeCheckbox = page.locator('//label[@for="tree-node-home"]');
+      const commandsCheckbox = page.locator(
+        '//label[@for="tree-node-commands"]'
+      );
+      const officeCheckbox = page.locator('//label[@for="tree-node-office"]');
+      const publicCheckbox = page.locator('//label[@for="tree-node-public"]');
+      const excelFileCheckbox = page.locator(
+        '//label[@for="tree-node-excelFile"]'
+      );
 
-      await page.locator("//button[@aria-label='Expand all']").click();
+      await page.goto("/checkbox");
+      await expect(checkboxTitle).toBeVisible();
+
+      await expandAllButton.click();
 
       // Check Home Checkbox
-      const homeCheckbox = page.locator('//label[@for="tree-node-home"]');
       await expect(homeCheckbox).not.toBeChecked();
       await homeCheckbox.click();
       await expect(homeCheckbox).toBeChecked();
 
       // Check Commands Checkbox
-      const commandsCheckbox = page.locator(
-        '//label[@for="tree-node-commands"]'
-      );
       await expect(commandsCheckbox).toBeChecked();
       await commandsCheckbox.click();
       await expect(commandsCheckbox).not.toBeChecked();
 
       // Check Office Checkbox
-      const officeCheckbox = page.locator('//label[@for="tree-node-office"]');
       await expect(officeCheckbox).toBeChecked();
       await officeCheckbox.click();
       await expect(officeCheckbox).not.toBeChecked();
-      await expect(
-        page.locator("//label[@for='tree-node-public']")
-      ).not.toBeChecked();
+      await expect(publicCheckbox).not.toBeChecked();
 
       // Check excelFile Checkbox
-      const excelFileCheckbox = page.locator(
-        '//label[@for="tree-node-excelFile"]'
-      );
       await expect(excelFileCheckbox).toBeChecked();
       await excelFileCheckbox.click();
       await expect(excelFileCheckbox).not.toBeChecked();
--- tests/demo-qa-site/radio-button.spec.ts
+++ tests/demo-qa-site/radio-button.spec.ts
@@ -4,33 +4,39 @@ test.describe(
   { tag: ["@smoke", "@regression", "@ui", "@functional", "@positive"] },
   async () => {
     test("OL-131 Check Radio Buttons with Xpath", async ({ page }) => {
+      const radioButtonTitle = page.locator(
+        "//h1[contains(text(),'Radio Button')]"
+      );
+      const yesRadioMarker = page.getByRole("radio", { name: "Yes" });
+      const yesLabel = page.locator('//label[@for="yesRadio"]');
+      const impressiveRadioMarker = page.locator(
+        '//input[@id="impressiveRadio"]'
+      );
+      const impressiveLabel = page.locator('//label[@for="impressiveRadio"]');
+      const noRadio = page.locator('//input[@id="noRadio"]');
+
       await page.goto("/radio-button");
-      await expect(
-        page.locator("//h1[contains(text(),'Radio Button')]")
-      ).toBeVisible();
+      await expect(radioButtonTitle).toBeVisible();
 
       // Check "Yes" radio button
-      const yesRadio = page.getByRole("radio", { name: "Yes" });
-      await expect(yesRadio).not.toBeChecked();
+      await expect(yesRadioMarker).not.toBeChecked();
 
-      await page.locator('//label[@for="yesRadio"]').click();
-      await expect(yesRadio).toBeChecked();
+      await yesLabel.click();
+      await expect(yesRadioMarker).toBeChecked();
       await expect(
         page.locator("//span[@class='text-success' and text()='Yes']")
       ).toBeVisible();
 
       // Check "Impressive" radio button
-      const impressiveRadio = page.locator('//input[@id="impressiveRadio"]');
-      await expect(impressiveRadio).not.toBeChecked();
-      await page.locator('//label[@for="impressiveRadio"]').click();
+      await expect(impressiveRadioMarker).not.toBeChecked();
+      await impressiveLabel.click();
 
-      await expect(impressiveRadio).toBeChecked();
+      await expect(impressiveRadioMarker).toBeChecked();
       await expect(
         page.locator("//span[@class='text-success' and text()='Impressive']")
       ).toBeVisible();
 
       // check that "No" radio button is disabled
-      const noRadio = page.locator('//input[@id="noRadio"]');
       await expect(noRadio).toBeDisabled();
       await expect(noRadio).toHaveClass(/disabled/);
     });
